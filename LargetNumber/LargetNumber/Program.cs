2e3794e2c976c0652a79284892d9e44f4604854d
bhargavak <19756773+kbhagi@users.noreply.github.com>
2020-03-15 12:18:14 +0530

    Add files via upload

---
+++ LargetNumber/LargetNumber/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LargestNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 5, 3, 7, 8, 9 };
+            int x = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (x > array[i])
+                {
+                    continue;
+                }
+                else
+                {
+                    x = array[i];
+                }
+            }
+            Console.WriteLine("Largest Number = " + x);
+            Console.ReadKey();
+        }
+    }
+}
